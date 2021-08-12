7cbfa57558b139d8bb2eef151d54d17a5f05c53c
Lukas <lukasindre@gmail.com>
2021-08-12 14:16:30 +0000

    tf fmt

--- rules.tf
+++ rules.tf
@@ -1,9 +1,9 @@
 resource "okta_group_rule" "gods_only" {
-name = "Gods Only Mapping"
-status = "ACTIVE"
-group_assignments = [
-local.all_groups["Gods Only"]["id"]
-]
-expression_type = "urn:okta:expression:1.0"
-expression_value = "isMemberOfAnyGroup(\"${local.all_groups["Engineering - IT"]["id"]}\")"
+  name   = "Gods Only Mapping"
+  status = "ACTIVE"
+  group_assignments = [
+    local.all_groups["Gods Only"]["id"]
+  ]
+  expression_type  = "urn:okta:expression:1.0"
+  expression_value = "isMemberOfAnyGroup(\"${local.all_groups["Engineering - IT"]["id"]}\")"
 }
